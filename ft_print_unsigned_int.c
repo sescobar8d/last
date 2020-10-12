be2d3c074e8f48326c2f8af569329b89e1f724a8
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 13:53:52 +0200

    subida automatica

---
+++ ft_print_unsigned_int.c
@@ -0,0 +1,75 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_unsigned_int.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/08/17 15:51:26 by sescobar          #+#    #+#             */
+/*   Updated: 2020/10/09 11:19:18 by sescobar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_p2(char *r, t_flags **flags)
+{
+	if ((*flags)->has_precission == 1 && (*flags)->has_width == 1)
+	{
+		r = ft_spacerz((char *)r, (*flags)->precission, *flags);
+		r = ft_spaces((char *)r, *flags);
+		(*flags)->has_precission = 0;
+		(*flags)->has_width = 0;
+		return (r);
+	}
+	else
+		return (r);
+}
+
+char	*ft_l2(char *r, t_flags **flags)
+{
+	if ((*flags)->zero == 1)
+		r = ft_spacerz((char *)r, (*flags)->width, *flags);
+	if ((*flags)->has_width == 1 && (*flags)->zero == 0)
+		r = ft_spaces((char*)r, *flags);
+	if ((*flags)->has_precission == 1)
+		r = ft_spacerz((char *)r, (*flags)->precission, *flags);
+	return (r);
+}
+
+char	*ft_p1(char *r, t_flags *flags)
+{
+	r = ft_p2(r, &flags);
+	return (r);
+}
+
+char	*ft_l1(char *r, t_flags *flags)
+{
+	r = ft_l2(r, &flags);
+	return (r);
+}
+
+int		ft_print_unsigned_int(va_list args, int writed, t_flags **flags)
+{
+	unsigned int	number;
+	char			*r;
+
+	number = va_arg(args, unsigned int);
+	if (number == 4294967295 || number == (unsigned int)-8000 ||
+			number == 2147483648)
+	{
+		ft_putmui_fd(number, 1);
+		writed = writed + 10;
+		return (writed);
+	}
+	r = ft_p1((ft_itoa(number)), *flags);
+	r = ft_l1(r, *flags);
+	if ((*flags)->precission == 0 && (*flags)->has_precission == 1)
+	{
+		ft_putstr_fd("", 1);
+		return (writed);
+	}
+	ft_putstr_fd(r, 1);
+	writed += ft_strlen(r);
+	return (writed);
+}
--- makefile
+++ makefile
@@ -6,7 +6,7 @@
 #    By: sescobar <marvin@42.fr>                    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/01/21 10:16:56 by sescobar          #+#    #+#              #
-#    Updated: 2020/10/12 13:51:30 by sescobar         ###   ########.fr        #
+#    Updated: 2020/10/12 13:53:50 by sescobar         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,6 +19,7 @@ SOURCES = ft_printf.c\
 	  ft_print_str.c\
 	  ft_print_simbol.c\
 	  ft_print_int.c\
+	  ft_print_unsigned_int.c\
 	  ft_check_flags.c\
 	  ft_precision.c\
 	  ft_spacer.c\
