3303338937211be50c220644939960264d61c4a3
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 19:54:12 +0200

    subida automatica

--- ft_print_unsigned_int.c
+++ ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:26 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/12 19:53:27 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/12 19:54:09 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,7 +63,7 @@ int		ft_print_unsigned_int(va_list args, int writed, t_flags **flags)
 		return (writed);
 	}
 	r = ft_p1(/*(ft_itoa(number))*/"hola", *flags);
-	r = ft_l1(r, *flags);
+	//r = ft_l1(r, *flags);
 	//r = ft_l1(ft_p1((ft_itoa(number)), *flags), *flags);
 	if ((*flags)->precission == 0 && (*flags)->has_precission == 1)
 	{
