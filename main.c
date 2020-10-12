ae5150d9623d3070d8c1482256907726765a82b8
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 11:30:11 +0200

    subida automatica

--- main.c
+++ main.c
@@ -45,7 +45,7 @@ int main(void) {
 	ft_printf("%jd\\n", 9223372036854775807);
 	ft_printf("%zd\\n", 4294967295);
 	ft_printf("%\\n");
-	/*ft_printf("%U\\n", 4294967295);
+	ft_printf("%U\\n", 4294967295);
 	ft_printf("%o\\n", 40);
 	ft_printf("%%#08x\\n", 42);
 	ft_printf("%x\\n", 1000);
@@ -785,6 +785,6 @@ int main(void) {
 	printf("p: %p %p %p %p %p %p %p %p %p %p %p %p\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 	printf("x: %x %x %x %x %x %x %x %x %x %x %x %x\n", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 //	ft_printf("%09s", "hi Low");
-	*/system("leaks -fullContent  a.out");
+	system("leaks -fullContent  a.out");
 	return 0;
 }
