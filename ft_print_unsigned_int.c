db306472ff022312d9f29e9afe7ac7ec2d3fda48
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 15:44:44 +0200

    subida automatica

--- ft_print_unsigned_int.c
+++ ft_print_unsigned_int.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:26 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/12 15:44:12 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/12 15:44:42 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,6 +71,6 @@ int		ft_print_unsigned_int(va_list args, int writed, t_flags **flags)
 	}
 	ft_putstr_fd(r, 1);
 	writed += ft_strlen(r);
-	free(r);
+//	free(r);
 	return (writed);
 }
