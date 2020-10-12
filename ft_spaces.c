95b383bd717d6831914cc781f6aba9a8d6909e85
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 13:49:27 +0200

    subida automatica

--- ft_spaces.c
+++ ft_spaces.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:48 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/12 13:48:30 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/12 13:49:23 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,12 +49,12 @@ char	*ft_spaces(char *string, t_flags *flags)
 	}
 	if ((flags)->minus == 1)
 	{
-		spaces = ft_strjoin(string, spaces);
+		spaces = ft_strjoin_free(string, spaces, 3);
 		return (spaces);
 	}
 	if ((flags)->space < 0)
 		return (string);
-	spaces = ft_strjoin(spaces, string);
+	spaces = ft_strjoin_free(spaces, string, 3);
 	return (spaces);
 }
 
