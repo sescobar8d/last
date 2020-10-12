7f496ff48280f8d69be0ffa0c31a80108b1782ea
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 13:58:18 +0200

    subida automatica

--- ft_spacer.c
+++ ft_spacer.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:39 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/12 13:57:14 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/12 13:58:16 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,7 +67,7 @@ char	*ft_spacerz(char *str, int n, t_flags *flags)
 		if ((flags)->has_precission == 1 && (flags)->has_width == 1)
 		{
 			ft_spacerc(result, ((n - c) - ft_strlen(str)));
-			result = ft_strjoin_free((ft_strjoin_free("-", result, 2)), str, 1);
+			result = ft_strjoin_free((ft_strjoin_free("-", result, 2)), str, 3);
 			return (result);
 		}
 		ft_putstr_fd("-", 1);
