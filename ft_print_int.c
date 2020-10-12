c4460fcf5b8587050c5f2fb57e928bc2d8de5221
sescobar <santiagoescobarrozo@gmail.com>
2020-10-12 13:21:47 +0200

    subida automatica

--- ft_print_int.c
+++ ft_print_int.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:50:47 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/12 13:20:26 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/12 13:21:45 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,8 +68,8 @@ int		ft_print_int(va_list args, int writed, t_flags **flags)
 	number = va_arg(args, int);
 	r = ft_itoa(number);
 	t = ft_strjoin_free(r, "", 1);
-	/*r = ft_f1(r, number, *flags);
-	if ((*flags)->has_precission == 1 && (*flags)->precission == 0
+	r = ft_f1(t, number, *flags);
+	/*if ((*flags)->has_precission == 1 && (*flags)->precission == 0
 		&& (*flags)->width == 0)
 	{
 		ft_putstr_fd("", 1);
