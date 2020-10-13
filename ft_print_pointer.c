70a895ba8ad8a95c7d8164db3b302c516f11aaa2
sescobar <santiagoescobarrozo@gmail.com>
2020-10-13 13:55:49 +0200

    subida automatica

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:50:39 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/13 13:55:20 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/13 13:55:48 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int			ft_print_pointer(const char *str, va_list args,
 	char			*hex;
 
 	n = va_arg(args, unsigned long);
-	if (!p)
+	if (!n)
 		hex = ft_strjoin_free(hex, "0x0", 1);
 	if ((((*flags)->has_precission == 1 && (*flags)->precission == 0) ||
 		((*flags)->has_width == 1 && (*flags)->width == 0)) && (n == 0))
