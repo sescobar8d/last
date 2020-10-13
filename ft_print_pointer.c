b248a93f3c5a6c9194fe06a9f9a618be868862d6
sescobar <santiagoescobarrozo@gmail.com>
2020-10-13 14:11:12 +0200

    subida automatica

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:50:39 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/13 14:09:49 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/13 14:11:10 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,16 +54,19 @@ int			ft_print_pointer(va_list args,
 	hex = NULL;
 	if (!n)
 		hex = ft_strjoin_free(hex, "0x0", 1);
-	if ((((*flags)->has_precission == 1 && (*flags)->precission == 0) ||
-		((*flags)->has_width == 1 && (*flags)->width == 0)) && (n == 0))
-		hex = ft_strjoin_free("", "", 0);
 	else
-		hex = ft_ul_to_hex(n);
-	if ((*flags)->hash == 1 && (*flags)->zero == 1 && (*flags)->has_width == 1
+	{
+		if ((((*flags)->has_precission == 1 && (*flags)->precission == 0) ||
+			((*flags)->has_width == 1 && (*flags)->width == 0)) && (n == 0))
+				hex = ft_strjoin_free("", "", 0);
+		else
+			hex = ft_ul_to_hex(n);
+		if ((*flags)->hash == 1 && (*flags)->zero == 1 && (*flags)->has_width == 1
 		&& (*flags)->has_precission == 0 && ((*flags)->width > ft_strlen(hex)))
-		hex = ft_strjoin_free("0", hex, 2);
-	if ((*flags)->hash == 1 && n != 0)
-		hex = ft_strjoin_free("0x", hex, 2);
+			hex = ft_strjoin_free("0", hex, 2);
+		if ((*flags)->hash == 1 && n != 0)
+			hex = ft_strjoin_free("0x", hex, 2);
+	}
 	hex = precission_p(hex, flags);
 	hex = width_p(hex, flags);
 	ft_putstr_fd(hex, 1);
