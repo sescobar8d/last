e45234febc7630855c78c01e70e3cb6a8ae71d30
sescobar <santiagoescobarrozo@gmail.com>
2020-10-13 14:34:01 +0200

    subida automatica

--- ft_print_pointer.c
+++ ft_print_pointer.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:50:39 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/13 14:33:20 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/13 14:34:00 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,12 +46,12 @@ char		*width_p(char *hex, t_flags **flags)
 
 char    *checkz(char *hex, t_flags *flags)
 {
-        //hex = ft_strjoin_free(hex, "0x0", 1);
+        hex = ft_strjoin_free(hex, "0x0", 1);
         if (flags->has_precission == 1 && flags->precission == 0)
                 hex = ft_strjoin_free("", "", 0);
         if ((flags)->has_precission == 1)
                 hex = precission_p(hex, &flags);
-        hex = width_p(hex, &flags);
+       // hex = width_p(hex, &flags);
         return (hex);
 }
 
