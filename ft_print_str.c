57160275d51389067a735ca124a0bd1b245d477f
sescobar <santiagoescobarrozo@gmail.com>
2020-10-14 12:40:56 +0200

    subida automatica

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:16 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/14 12:39:34 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/14 12:40:54 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 		ft_delete(*flags);
 	}
 	string = ld(string, *flags);
-	*///ft_putstr_fd(string, 1);
+	*/ft_putstr_fd(string, 1);
 	//writed += ft_strlen(string);	
 	return (writed);
 }
