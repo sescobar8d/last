28529fa9c523e7ddfed1e85c75321327f427faf6
sescobar <santiagoescobarrozo@gmail.com>
2020-10-15 11:32:49 +0200

    subida automatica

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:16 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/15 11:27:11 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/15 11:32:47 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 {
 	char	*string;
 
-	string = va_arg(args, char *);
+	string = va_arg(args, char*);
 	/*if (string == NULL)
 	{
 		string = "(null)";
@@ -70,7 +70,6 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 	string = ld(string, *flags);
 	ft_putstr_fd(string, 1);
 	*/
-	string[0] = '\0';
 	writed += ft_strlen(string);	
 	return (writed);
 }
