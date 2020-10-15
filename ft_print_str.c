ef0d6c854b484847afd276366cdb91c7d78ec578
sescobar <santiagoescobarrozo@gmail.com>
2020-10-15 11:40:04 +0200

    subida automatica

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:16 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/15 11:38:22 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/15 11:39:24 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,14 +54,14 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 	char	*string;
 
 	string = va_arg(args, char *);
-	if (string == NULL)
+	/*if (string == NULL)
 	{
 		string = "(null)";
-		/*if ((*flags)->has_precission == 1 && (*flags)->precission > 0)
+		if ((*flags)->has_precission == 1 && (*flags)->precission > 0)
 			string = ft_precision_str((char *)string, *flags);
 		string = ft_spaces_str(string, *flags);
-*/	}
-	/*else if ((*flags)->has_width == 1 && (*flags)->has_precission == 1)
+	}
+	else if ((*flags)->has_width == 1 && (*flags)->has_precission == 1)
 	{
 		string = ft_precision_str((char *)string, *flags);
 		string = ft_spaces_str((char *)string, *flags);
@@ -69,6 +69,7 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 	}
 	string = ld(string, *flags);
 	ft_putstr_fd(string, 1);
-	*/writed += ft_strlen(string);	
+	writed += ft_strlen(string);*/
+	ft_putstr_fd(string, 1);
 	return (writed);
 }
