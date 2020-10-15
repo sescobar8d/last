ab02150e9d1b04670affd6e2dc56744a9fb3a28b
sescobar <santiagoescobarrozo@gmail.com>
2020-10-15 11:11:24 +0200

    subida automatica

--- ft_str1.c
+++ ft_str1.c
@@ -12,7 +12,7 @@ char    *ft_precision_str(char *string, t_flags *flags)
                 (flags)->precission == 0)
                 string = "";
         result = ft_calloc(1, (flags)->precission);
-	ft_strlcat(result, string, (flags)->precission + 1);
+	//ft_strlcat(result, string, (flags)->precission + 1);
 	//result = ft_strjoin_free(result, f, 3);
 	return (result);
 }
