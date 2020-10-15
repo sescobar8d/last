cb3534af9ec43ecb0acfd25c168e1e20b31b3652
sescobar <santiagoescobarrozo@gmail.com>
2020-10-15 11:27:13 +0200

    subida automatica

--- ft_print_str.c
+++ ft_print_str.c
@@ -6,7 +6,7 @@
 /*   By: sescobar <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/17 15:51:16 by sescobar          #+#    #+#             */
-/*   Updated: 2020/10/15 11:26:48 by sescobar         ###   ########.fr       */
+/*   Updated: 2020/10/15 11:27:11 by sescobar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,7 +70,7 @@ int		ft_print_str(va_list args, int writed, t_flags **flags)
 	string = ld(string, *flags);
 	ft_putstr_fd(string, 1);
 	*/
-	string[(*flags)->precission] = '\0';
+	string[0] = '\0';
 	writed += ft_strlen(string);	
 	return (writed);
 }
