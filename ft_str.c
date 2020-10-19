2983ecaaf41357a773ca4521f120523846a99a56
sescobar <santiagoescobarrozo@gmail.com>
2020-10-19 13:27:55 +0200

    subida automatica

--- ft_str.c
+++ ft_str.c
@@ -46,6 +46,7 @@ char    *ft_spaces_str(char *string, t_flags *flags)
         {
                 res = ft_strjoin(string, spaces);
 		free(spaces);
+		free(string);
                 return (res);
         }
         if ((flags)->space < 0)
