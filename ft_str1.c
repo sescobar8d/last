71b26f7dfa3962c576e7dc7d0cd3e207f3cd2426
sescobar <santiagoescobarrozo@gmail.com>
2020-10-20 10:23:24 +0200

    subida automatica

--- ft_str1.c
+++ ft_str1.c
@@ -73,7 +73,7 @@ char    *ft_spacerz_str(char *str, int n, t_flags *flags)
         if (ft_strlen(str) > (unsigned int)n)
                 return (str);
         ft_spacerc_str(result, ((n - c) - ft_strlen(str)));
-        d = ft_strjoin_free(result, str, 3);
+        d = ft_strjoin_free(result, str, 1);
         return (d);
 }
 
